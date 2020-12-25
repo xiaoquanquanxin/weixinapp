d51cdbe5554a0c0f647173d385f692056eef84dd
quanxin <quanxin@pujitech.com>
2020-12-25 15:14:03 +0800

    格式化

--- src/Components/Phasetwo/CityInformation/List/Component.js
+++ src/Components/Phasetwo/CityInformation/List/Component.js
@@ -28,7 +28,7 @@ export default class PhasetwoCityInformationList extends React.Component {
        // console.log(111,CityListdata)
         //console.log("22222", [[...provincesList], [...cityList]]);
         //const {tip}=storeTemplate;
-        
+
         return <div className={"Components-CityInformationList-container"}>
             <div className="header">
                 <div className="headerItem">
@@ -74,19 +74,19 @@ export default class PhasetwoCityInformationList extends React.Component {
 
                     })
                 }
-                {/* <div className={"item item2"}>
-                    <div className={"right"}>
-                        <div className={"title"}>根治城市病得先治“九龙治水”</div>
-                        <div className={"text"}>“管理的碎片化，思想的碎片化，这是影响我们城市建设最大问题。比如地下空间的开发就是各搞各的，有人防工程，有地铁，还有市政管廊。”</div>
-                    </div>
-                    <div className={"img3"}>
-                        <div className={"img"}><img src={listimg1} /></div>
-                        <div className={"img"}><img src={listimg1} /></div>
-                        <div className={"img"}><img src={listimg1} /></div>
-                    </div>
-                    <div className={"date"}>2019-04-20</div>
-                    
-                </div> */}
+                {/*<div className={"item item2"}>*/}
+                {/*    <div className={"right"}>*/}
+                {/*        <div className={"title"}>根治城市病得先治“九龙治水”</div>*/}
+                {/*        <div className={"text"}>“管理的碎片化，思想的碎片化，这是影响我们城市建设最大问题。比如地下空间的开发就是各搞各的，有人防工程，有地铁，还有市政管廊。”</div>*/}
+                {/*    </div>*/}
+                {/*    <div className={"img3"}>*/}
+                {/*        <div className={"img"}><img src='https://img-blog.csdnimg.cn/20201014180756919.png' /></div>*/}
+                {/*        <div className={"img"}><img src='https://img-blog.csdnimg.cn/20201014180756919.png'/></div>*/}
+                {/*        <div className={"img"}><img src='https://img-blog.csdnimg.cn/20201014180756919.png'/></div>*/}
+                {/*        <div className={"img"}><img src='https://img-blog.csdnimg.cn/20201014180756919.png'/></div>*/}
+                {/*    </div>*/}
+                {/*    <div className={"date"}>2019-04-20</div>*/}
+                {/*</div>*/}
             </div>
                 {CityListdata && CityListdata.length == 0 ? < div className={"actbottomgray"}>暂无数据</div> :
                     actbottom == 1 ? <div className={"actbottom"}>已经到底部</div> : <div className={"actbottom"}>拉动刷新数据</div>
