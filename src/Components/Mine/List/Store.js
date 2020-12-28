5df1d25b6fa435a72da7d7484126531574293ebc
quanxin <quanxin@pujitech.com>
2020-12-28 16:32:38 +0800

    格式化

--- src/Components/Mine/List/Store.js
+++ src/Components/Mine/List/Store.js
@@ -7,6 +7,7 @@ import myActivity from './img/my-activity.png';
 import myQuestionnaire from './img/my-questionnaire.png';
 import myRepair from './img/my-repair.png';
 import changePhone from './img/change-phone.png';
+
 // 定义数据结构
 class Store {
     //使用 observable decorator
@@ -21,14 +22,42 @@ class Store {
     @observable list2 = [
         {ico: myActivity, test: "我的活动", link: "/PhasetwoActivitySignUpList",},
         {ico: myQuestionnaire, test: "我的问卷", link: "/PhasetwoMyQuestionnaireList/1"},
-        {ico: myRepair, test: '我的报修', link: "xxx"}
+        {ico: myRepair, test: '我的报修', link: router.RepairList[0]},
     ];
     @observable list3 = [
-        {ico: changePhone, test: "更换号码", type: 'changePhone'}
+        {ico: changePhone, test: "更换号码  √", type: 'changePhone'}
     ];
     @observable type = "blue";
     @observable label = "退出";
-    @observable useInfo = {};
+    @observable useInfo = {
+        //  可能的key-value
+        // "id": 9,
+        // "authStatus": 0,
+        // "authTime": null,
+        // "birthday": null,
+        // "createTime": "2020-12-28 13:56:49",
+        // "createUserId": null,
+        // "deleteStatus": 1,
+        // "disabledStatus": 1,
+        // "firstFocusTime": "2020-07-10 15:08:42",
+        // "focusStatus": 1,
+        // "focusTime": "2020-07-10 15:08:42",
+        // "fullName": null,
+        // "identityNo": null,
+        // "nickName": "权鑫",
+        // "openId": "ouxLS1G1Y2ZMQ81vz3KZJe0oyieQ",
+        // "phoneNo": null,
+        // "sex": 1,
+        // "unionId": null,
+        // "updateTime": "2020-12-28 13:56:58",
+        // "updateUserId": null,
+        // "userLogo": "http://wx-life.seedland.cc/file-service/other/20201228//91148ed312314334b0b179ca54801d1f.png",
+        // "wxNo": null,
+        // "wxSubId": 3,
+        // "country": "中国",
+        // "province": "",
+        // "city": ""
+    }
 }
 
 export default Store;
