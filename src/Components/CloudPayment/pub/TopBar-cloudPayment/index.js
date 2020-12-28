0bf48eaec7f447ff395f287b431905e59b4cc210
quanxin <quanxin@pujitech.com>
2020-12-28 15:55:41 +0800

    Store.js

--- src/Components/CloudPayment/pub/TopBar-cloudPayment/index.js
+++ src/Components/CloudPayment/pub/TopBar-cloudPayment/index.js
@@ -7,49 +7,54 @@ import './Component.less'
 /*自定义类*/
 
 
- export default class TopBar extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state={
-		}
-	}
-	static propTypes = {
-		// label: PropTypes.string.isRequired, //标签
-		// type: PropTypes.string.isRequired,//类型
-	};
+export default class TopBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
 
-	 goback = () =>{
-	 	const href=window.location.href;
-	 	const pageName_DechanHandle_=href.includes('/CloudPayment/DechanHandle');
-		const pageName_Wuyehandle_=href.includes('/CloudPayment/Wuyehandle');
-		if (pageName_DechanHandle_ || pageName_Wuyehandle_) {
-			let uObj = window.getQueryString();
+    static propTypes = {
+        // label: PropTypes.string.isRequired, //标签
+        // type: PropTypes.string.isRequired,//类型
+    };
 
-			if (uObj.orderDetailId) {
-				console.log(uObj.orderDetailId);
-				this.props.hostry.push('/CloudPayment/DeliveryAppointment/?orderDetailId='+uObj.orderDetailId)
-				return false
-			}
-			this.props.hostry.push('/CloudPayment/DeliveryAppointment/')
+    goback = () => {
+        const href = window.location.href;
+        const pageName_DechanHandle_ = href.includes('/CloudPayment/DechanHandle');
+        const pageName_Wuyehandle_ = href.includes('/CloudPayment/Wuyehandle');
+        if (pageName_DechanHandle_ || pageName_Wuyehandle_) {
+            let uObj = window.getQueryString();
 
-		} else {
-			this.props.hostry.goBack()
-		}
-	 };
+            if (uObj.orderDetailId) {
+                console.log(uObj.orderDetailId);
+                this.props.hostry.push('/CloudPayment/DeliveryAppointment/?orderDetailId=' + uObj.orderDetailId)
+                return false
+            }
+            this.props.hostry.push('/CloudPayment/DeliveryAppointment/')
 
-	render() {
-		let {title,right,lineHeightValue}=this.props;
-		return (<div className="Components-TopBar-container">
-			<div className={'topbar'} style={{lineHeight: window.OSInfo()=='android'? lineHeightValue+'px': 'auto', backgroundColor: (this.props.opacity==1 || !this.props.opacity)  ?'#1A2C52': 'transparent'}}>
-				{
-					this.props.left ? <div style={{width:45}}> &nbsp;</div>: <div className="item" onClick={this.goback}> <span className="icon font_family"> &#xe662;</span></div>
-				}
+        } else {
+            this.props.hostry.goBack()
+        }
+    };
 
-				<div className="item" >{title}</div>
-				{right && <div className="item">{right.props.children}</div>}
+    render(){
+        let {title, right, lineHeightValue} = this.props;
+        return (<div className="Components-TopBar-container">
+            <div className={'topbar'} style={{
+                lineHeight: window.OSInfo() == 'android' ? lineHeightValue + 'px' : 'auto',
+                background: (this.props.opacity == 1 || !this.props.opacity) ? 'linear-gradient(to right, #04639D, #024868)' : 'transparent'
+            }}>
+                {
+                    this.props.left ? <div style={{width: 45}}> &nbsp;</div> :
+                        <div className="item" onClick={this.goback}><span className="icon font_family"> &#xe662;</span>
+                        </div>
+                }
 
-			</div>
-            <div className={'topbarHeight'}> </div>
-		</div>)
-	}
+                <div className="item">{title}</div>
+                {right && <div className="item">{right.props.children}</div>}
+
+            </div>
+            <div className={'topbarHeight'}></div>
+        </div>)
+    }
 }
