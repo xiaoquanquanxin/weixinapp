03c6ead1af79f4a67c51c903ce33455c428b8458
quanxin <quanxin@pujitech.com>
2021-01-05 19:21:50 +0800

    title

---
+++ src/Components/wechatPay/Prepayment/index.js
@@ -0,0 +1,21 @@
+import lazyLoadComponent from "lazy-load-component";
+import Actions from "./Actions";
+import Store from "./Store";
+
+const Component = lazyLoadComponent(() => import(/* webpackChunkName: "Prepayment" */ './Component'));
+
+function compose({store, actions, routeList, name, routePath}){
+    let _store = new Store;
+    name = name.substring(0, 1).toUpperCase() + name.substring(1);
+    store['store' + name] = _store;
+    actions['actions' + name] = new Actions(_store);
+    let key = routeList.length;
+    routeList.push({
+        key,
+        exact: true,
+        component: Component,
+        path: routePath
+    })
+}
+
+export {compose, Component}
