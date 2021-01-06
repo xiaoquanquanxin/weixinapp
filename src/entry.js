5fa55c861839b8c2d9220d3fbad19f8dcbb768c7
quanxin <quanxin@pujitech.com>
2021-01-06 14:35:47 +0800

    title

--- src/entry.js
+++ src/entry.js
@@ -2,25 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import HashRouter from 'react-router-dom/HashRouter'
 import AppContainer from 'react-hot-loader/lib/AppContainer'
+
 const FastClick = require('../lib/fastclick')
 require('es6-promise').polyfill();
 //目前用的是jquery,感觉稳定很多
 //require('isomorphic-fetch');
 require('./global.js');
 //解决移动端300毫秒延迟
-FastClick.attach(document.body)
+FastClick.attach(document.body);
 import App from './App'
+
 const render = Component =>
     ReactDOM.render(
-            <AppContainer>
-                <HashRouter>
-                <Component />
-                </HashRouter>
-            </AppContainer>,
+        <AppContainer>
+            <HashRouter>
+                <Component/>
+            </HashRouter>
+        </AppContainer>,
         document.getElementById('root')
     );
 render(App);
-if(module.hot) {
+if (module.hot) {
     module.hot.accept('./App', () => {
         const NextRootContainer = require('./App').default;
         render(NextRootContainer)
