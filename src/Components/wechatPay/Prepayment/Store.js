80a1c3481c473d90d9634c2564d581257900fa5e
quanxin <quanxin@pujitech.com>
2021-01-07 15:52:48 +0800

    glob

--- src/Components/wechatPay/Prepayment/Store.js
+++ src/Components/wechatPay/Prepayment/Store.js
@@ -1,45 +1,48 @@
-import {observable, action} from 'mobx';
+import {observable} from 'mobx';
 
-// 定义数据结构
+//  定义数据结构
 class Store {
-    // 收费标准
-    @observable rates = false;
-    // 自定义
-    @observable custom = false;
-    @observable feeName = "";
-    // 费项列表
-    @observable feeInfo = [];
-    // 房间列表
+    //  收费标准是否展示
+    @observable ratesPickerShow = false;
+    //  自定义弹框
+    @observable customPickerShow = false;
+
+    //  房间列表
     @observable roomList = [];
-    @observable roomName = "实地-遵义蔷薇国际";
-    @observable feeId = '';
-    // 数据来源：房间号、表具编号、车位号
-    @observable itemsourcename = "";
-    // 专项预缴费项订单明细列表
-    @observable feeItems = [];
-    // 房间id
-    @observable roomID = "4a7477c8-7a28-46ce-bfc9-678e6dd71aaa";
-    // 是否有欠缴
-    @observable isFrozen = 0;
-    // 账单周期模型
-    @observable calcTimeUint = "";
-    // 允许缴纳最大月数
-    @observable maxMonth = 12;
-    // 快捷支付列表
-    @observable paymentList = [];
-    // 费项余额
-    @observable balanceAmount = "";
-    // 暂存款日期
-    @observable enoughDeductionDate = "";
-    // 收费标准
+    //  当前选择的房间
+    @observable currentRoom = {};
+
+    //  费项列表
+    @observable feeList = [];
+    //  当前选择的费项
+    @observable currentFee = {};
+
+    //  收费标准列表
     @observable feeCharge = [];
-    @observable customObj = {
-        type: 1,
-        checked: null,
-        paymentMonth: null,
-        perUnit: null,
+
+    //  queryFeeitemDetails接口的数据
+    @observable queryFeeitemDetails = {
+        //  是否有欠缴账单 0 没有欠缴账单 1有欠缴账单
+        hasOutstandingBill: 1,
+        //  允许缴纳最大月数
+        maxMonth: 0,
+        //  暂存款扣至日期
+        enoughDeductionDate: '',
+        //  暂存款余额
+        balanceAmount: 0,
+        //  预缴费项明细-专项预缴费项订单明细列表
+        feeItems: [],
     };
 
+    //  快捷支付列表-渲染的主要的块
+    @observable paymentList = [];
+    //  被选中的index
+    @observable activeIndex = -1;
+    //  自定义块
+    @observable customFeeItem = {
+        paymentMonth: 0,
+        perUnit: 0,
+    };
 }
 
 export default Store;
