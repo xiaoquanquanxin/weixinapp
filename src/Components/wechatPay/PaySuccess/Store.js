8f6551dfda66da44f8f97bffe956d465afefc357
quanxin <quanxin@pujitech.com>
2021-01-11 10:37:47 +0800

    glob

---
+++ src/Components/wechatPay/PaySuccess/Store.js
@@ -0,0 +1,17 @@
+import {observable} from 'mobx';
+
+// 定义数据结构
+class Store {
+    //  房间列表
+    @observable params = {
+        //  0欠缴
+        type: 0,
+        orderId: "",
+        orderMoney: "",
+        createTime: "",
+    };
+
+}
+
+export default Store;
+
