395dc56cd9f6f4fad70aeb92f2a9c569dc8b58ed
quanxin <quanxin@pujitech.com>
2021-01-11 10:39:44 +0800

    glob

--- src/Components/wechatPay/PaySuccess/Actions.js
+++ src/Components/wechatPay/PaySuccess/Actions.js
@@ -1,6 +1,20 @@
+import {action} from "mobx";
+
 class Actions {
     constructor(store){
         this.store = store;
+    }
+
+    //  解析参数
+    @action
+    getParams(){
+        const {params} = this.store;
+        const _params = window.getQueryString();
+        params.orderId = _params.orderId;
+        params.type = _params.type;
+        params.orderMoney = _params.orderMoney;
+        params.createTime = _params.createTime;
+        console.log('url params:', this.store.params);
     }
 }
 
