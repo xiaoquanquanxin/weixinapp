a5997ea6d1ad3318d083f62148f7ddd65516fb57
quanxin <quanxin@pujitech.com>
2021-01-11 18:00:26 +0800

    glob

--- src/Components/RepairYu/ComplaintSuggestions/Actions.js
+++ src/Components/RepairYu/ComplaintSuggestions/Actions.js
@@ -1,8 +1,21 @@
 //import {action } from 'mobx';
 // 定义对数据的操作
+import {ipUri} from "../../../config";
+
 class Actions {
-    constructor(store) {
+    constructor(store){
         this.store = store;
     }
+
+    //  获取电话数据
+    async areaPhoneList(){
+        const store = this.store;
+        let result = await window.GET({url: '/user/maintain/areaPhoneList'});
+        if (!result.isSucess) {
+            return;
+        }
+        store.phoneList = result.data;
+    }
 }
+
 export default Actions;
