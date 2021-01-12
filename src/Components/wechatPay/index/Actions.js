6dd126b578fb69e7923fff1c6dc6ec5a6c85343d
quanxin <quanxin@pujitech.com>
2021-01-12 18:56:51 +0800

    glob

--- src/Components/wechatPay/index/Actions.js
+++ src/Components/wechatPay/index/Actions.js
@@ -1,6 +1,23 @@
+import {action} from "mobx";
+
 class Actions {
-    constructor(store) {
+    constructor(store){
         this.store = store;
     }
+
+    /*
+    获取用户信息
+    * */
+    @action
+    userInfo = async () => {
+        let url = `user/userInfo`;
+        let cformData = {};
+        //let cformData = config.format(obj);
+        let result = await window.GET({url, cformData});
+        this.store.useInfo = result.data;
+        window.setLocalData('userInfo', result.data);
+        return result.resultCode;
+    }
 }
+
 export default Actions;
