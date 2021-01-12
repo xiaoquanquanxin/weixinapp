b67f6ef9d3bac8993ce1513527c8f6f7bacb8a97
quanxin <quanxin@pujitech.com>
2021-01-12 16:46:42 +0800

    glob

--- src/Components/RepairYu/AddRepair/Store.js
+++ src/Components/RepairYu/AddRepair/Store.js
@@ -1,34 +1,15 @@
 import {observable, action} from 'mobx';
-//          定义数据结构
-import temp from './temp.png';
-import temp2 from './11.jpg';
-
 class Store {
-    //上传图片列表
-    @observable array = [temp, temp2, temp, temp2];
-    @observable type = "";//状态 1.报修 2.投诉
     //  图片地址，多个以逗号分隔
     @observable imageCollection = "";
     @observable recordCollection = "";
-    //单选框数据
-    @observable radioData = [];
-    //认证信息
-    @observable userAuthInfo = {};
-    @observable colorStyle = false;
 
-
-
-    /***
-     * 以下
-     * 以下
-     * **/
+    //  验证成功
+    @observable isValidated = false;
     //  房间列表
     @observable roomList = [];
     //  房间下的客户列表
     @observable contactList = [];
-
-    //  预约时间
-    @observable appointmentTime = new Date();
 
     //  添加报修
     @observable AddRepair = {
@@ -42,10 +23,11 @@ class Store {
         //  手机号
         phoneNo: '',
 
-        //type: "",//1-报事，2-投诉
+        //  预约时间
+        appointmentTime: new Date(),
 
-        appealNature: "",//室内1；室外0；投诉2；咨询3；建议4；表扬5
-        problemDescription: "",//问题描述
+        //  问题描述
+        problemDescription: "",
 
     };
 }
