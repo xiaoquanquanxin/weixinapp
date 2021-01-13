425e12cb21b45e8334568f247e3d6453e280c79a
quanxin <quanxin@pujitech.com>
2021-01-13 19:30:52 +0800

    glob

--- src/global.js
+++ src/global.js
@@ -176,12 +176,12 @@ window.setWindowTitle = function (title){
     console.log('设置标题为：', title);
     window.document.title = title;
     const iframe = document.createElement('iframe');
-    iframe.src = '';
     iframe.style.display = 'none';
     iframe.onload = iframe.onerror = function (e){
         console.log(e);
         document.body.removeChild(iframe);
     };
+    iframe.src = '';
     document.body.appendChild(iframe);
     //  解决输入框弹出，页面滚动的bug,点击不了弹框。
     window.JQ('input').on('blur', function (){
