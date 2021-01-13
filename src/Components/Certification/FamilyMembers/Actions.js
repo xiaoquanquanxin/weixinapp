773301db16f497a11634842988cda8787b2cff1a
quanxin <quanxin@pujitech.com>
2021-01-13 15:25:05 +0800

    glob

--- src/Components/Certification/FamilyMembers/Actions.js
+++ src/Components/Certification/FamilyMembers/Actions.js
@@ -8,56 +8,75 @@ class Actions {
         this.store = store;
     }
 
+    //  初始化
+    init(){
+
+    }
+
     /*
       获取用户信息
       * */
     @action
     userInfo = async () => {
-
+        const store = this.store;
         let result = await window.GET({url: 'user/userInfo',});
-        if (!result.isSucess) return;
-        this.store.useInfo = result.data;
-        // console.log("useInfo11111111111", this.store.useInfo)
-
+        if (!result.isSucess) {
+            return;
+        }
+        store.useInfo = result.data;
         let cformData = {userType: 0};
         let resultgetRoomInfo = await window.GET({url: 'auth/getRoomInfo', cformData});
-        if (!resultgetRoomInfo.isSucess) return;
-        for (let i = 0; resultgetRoomInfo.data.length > i; i++) {
-            if (!this.store.getRoomInfo[i]) this.store.getRoomInfo[i] = {}
-            this.store.getRoomInfo[i].label = resultgetRoomInfo.data[i].roomName
-            this.store.getRoomInfo[i].value = resultgetRoomInfo.data[i].roomId
-            this.store.getRoomInfo[i].userType = resultgetRoomInfo.data[i].userType
+        if (!resultgetRoomInfo.isSucess) {
+            return;
         }
-        this.store.roomId = this.store.getRoomInfo[0].value;
-
-        this.topfunproject(this.store.roomId)
-        //console.log("getRoomInfo222222222", this.store.getRoomInfo, resultgetRoomInfo)
+        const {getRoomInfo} = store;
+        for (let i = 0; resultgetRoomInfo.data.length > i; i++) {
+            if (!getRoomInfo[i]) {
+                getRoomInfo[i] = {};
+            }
+            getRoomInfo[i].label = resultgetRoomInfo.data[i].roomName;
+            getRoomInfo[i].value = resultgetRoomInfo.data[i].roomId;
+            getRoomInfo[i].userType = resultgetRoomInfo.data[i].userType
+        }
+        console.log(JSON.parse(JSON.stringify(getRoomInfo)));
+        store.roomId = getRoomInfo[0].value;
+        this.topfunproject(store.roomId);
     };
     @action
     topfunproject = async (roomId) => {
-        this.store.getRoomInfo.forEach((item, index) => {
-            if (roomId == item.value) {
-                this.store.roomName = item.label;
-                this.store.userType = item.userType;
+        const store = this.store;
+        store.getRoomInfo.forEach((item, index) => {
+            if (roomId === item.value) {
+                store.roomName = item.label;
+                store.userType = item.userType;
+                store.roomId = roomId;
             }
         });
-        console.log(1090909111, this.store.roomName, this.store.userType);
-        let cformData = {roomId: roomId};
+        console.log('roomId', roomId);
+        console.log('roomName', store.roomName,);
+        console.log('userType', store.userType,);
+
+        let cformData = {roomId};
         let result = await window.GET({url: 'auth/userFamily', cformData});
-        if (!result.isSucess) return;
-        this.store.userFamily = [];
+        if (!result.isSucess) {
+            return;
+        }
+        store.userFamily = [];
+        //  过滤数据
         result.data.forEach((item, index) => {
-            this.store.userFamily.push({
-                id: item.memberId,
-                fullName: item.fullName,
-                sex: item.sex,
-                phoneNo: item.phoneNo,
-                identityNo: item.identityNo,
-                birthday: item.birthday,
-                userType: item.userType,
-                authUserId: item.authUserId,
-                editStatus: false
-            })
+            if (item.id && item.fullName && item.authUserId) {
+                store.userFamily.push({
+                    id: item.id,
+                    fullName: item.fullName,
+                    sex: item.sex,
+                    phoneNo: item.phoneNo,
+                    identityNo: item.identityNo,
+                    birthday: item.birthday,
+                    userType: item.userType,
+                    authUserId: item.authUserId,
+                    editStatus: false
+                })
+            }
         });
     };
 
@@ -68,7 +87,9 @@ class Actions {
     // 	let url=`auth/getRoomInfo`;
     // 	let cformData = { userType:0};
     // 	let result = await window.GET({ url:'auth/getRoomInfo',cformData});
-    // 	if (!result.isSucess) return;
+    // 	if (!result.isSucess) {
+    //         return;
+    //     }
     // 	this.store.getRoomInfo=result.data;
     // };
 
@@ -79,7 +100,9 @@ class Actions {
     // 	let obj = { ...body };
     // 	let cformData = config.format(obj);
     // 	let result=await window.GET({url,cformData});
-    // 	if (!result.isSucess) return;
+    // 	if (!result.isSucess) {
+    //         return;
+    //     }
     // 	const array=result.data;
     // 	array.forEach((item,index)=>{
     // 		this.store.userFamily.push({
@@ -107,7 +130,9 @@ class Actions {
             authUserId: authUserId
         };
         let result = await window.POST({url, cformData});
-        if (!result.isSucess) return;
+        if (!result.isSucess) {
+            return;
+        }
         //this.store.useInfo=result.data;
         return result.resultCode
     };
