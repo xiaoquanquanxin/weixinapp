2fc5ea475099bb07c16b76c0310552ff7706726c
quanxin <quanxin@pujitech.com>
2021-01-13 17:53:07 +0800

    glob

--- src/Components/Phasetwo/Activity/SignUpList/Actions.js
+++ src/Components/Phasetwo/Activity/SignUpList/Actions.js
@@ -1,38 +1,41 @@
-import {action } from 'mobx';
+import {action} from 'mobx';
+
 // 定义对数据的操作
 class Actions {
-    constructor(store) {
+    constructor(store){
         this.store = store;
         // this.activityTime="";
         // status
     }
+
     /*
     @action
     incA = () => {
         this.store.xxxxx++;
     }*/
     @action
-    Listfun = async()=>{
-        let cformData = { 
+    Listfun = async () => {
+        let cformData = {
             activityTime: this.store.timeValues,
             status: this.store.status
         };
-        console.log(5566,cformData)
-        let result = await window.GET({ url: 'user/activity/activitiApplyList', cformData });
+        console.log('活动列表的请求参数', cformData);
+        let result = await window.GET({url: 'user/activity/activitiApplyList', cformData});
         if (!result.isSucess) return;
         this.store.Listdata = result.data
-    }
+    };
     @action
-    statusfun=(v)=>{
-        console.log(v)
-        this.store.status= v[0]
+    statusfun = (status) => {
+        console.log(status);
+        this.store.status = status[0];
         this.Listfun()
-    }
+    };
     @action
     timefun = (v) => {
-        console.log(v)
-        this.store.timeValues= v
+        console.log(v);
+        this.store.timeValues = v;
         this.Listfun()
     }
 }
+
 export default Actions;
