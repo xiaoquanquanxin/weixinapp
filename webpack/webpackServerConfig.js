d99457000cebf5ef1e6f8783c8c8860a9a7fb23b
quanxin <quanxin@pujitech.com>
2021-01-14 15:04:24 +0800

    glob

--- webpack/webpackServerConfig.js
+++ webpack/webpackServerConfig.js
@@ -2,5 +2,8 @@ module.exports = {
     devServer: {
         host: '192.168.100.128',
         port: 8088,
+        proxy: {
+            "/life-web": 'http://asm-test.seedland.cc:8084/',
+        }
     },
 };
