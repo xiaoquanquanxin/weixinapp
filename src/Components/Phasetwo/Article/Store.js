9b8e7b732c4d88294eae61d19761657d5ccae809
quanxin <quanxin@pujitech.com>
2021-01-20 10:54:09 +0800

    glob

--- src/Components/Phasetwo/Article/Store.js
+++ src/Components/Phasetwo/Article/Store.js
@@ -5,10 +5,10 @@ class Store {
     //使用 observable decorator
     @observable tip = '公告页面';
     @observable setnoticeDetail = {
-        authorName: "实地",
-        content: "<p><span/></p>",
-        noticeTitle: "重庆蔷薇国际不利因素",
-        updateTime: "2019-07-02 14:47:47",
+        authorName: "",
+        content: "<p/>",
+        noticeTitle: "",
+        updateTime: "",
     };
 
     @observable contentdata = "";
