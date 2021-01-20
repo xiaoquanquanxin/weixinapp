5ac7232285306521dcbb5add5e8f00f52ee802f0
quanxin <quanxin@pujitech.com>
2021-01-20 17:23:08 +0800

    glob

--- src/Components/Phasetwo/Activity/UserList_new/Store.js
+++ src/Components/Phasetwo/Activity/UserList_new/Store.js
@@ -1,55 +1,48 @@
-import { observable, action } from 'mobx';
+import {observable, action} from 'mobx';
+
 // 定义数据结构
 class Store {
     //使用 observable decorator
-    @observable tip='我是模板';
-	@observable needCost=""
-/*获取用户信息*/
-	@observable useInfo={
-	};
+    @observable tip = '我是模板';
+    @observable needCost = "";
+    /*获取用户信息*/
+    @observable useInfo = {};
 
+    @observable userFamily = [
+        {
+            id: '',
+            fullName: '',
+            sex: '',
+            phoneNo: '',
+            identityNo: '',
+            birthday: '',
+            userType: '',
+            editStatus: false
+        },
+    ];
+    @observable getRoomInfo = [];
+    @observable roomId = -1;
+    @observable roomName = -1;
+    @observable userType = -1;
 
-	@observable userFamily=[
-		{
-			id: '',
-			fullName:'',
-			sex: '',
-			phoneNo: '',
-			identityNo: '',
-			birthday: '',
-			userType: '',
-			editStatus:false
-		},
-	];
-	@observable getRoomInfo = [
-	];
-	@observable roomId=-1
-	@observable roomName=-1
-	@observable userType=-1
+    @observable Interestselects = [];
+    @observable userList = [];
 
-	@observable Interestselects = []
-	@observable userList = []
+    @observable UserInfodata = [];
 
-	@observable UserInfodata=[]
-
-	@observable citylist = [{
-		"label": "北京市",
-		"value": "110000",
-		"children": [{
-			"label": "北京市",
-			"value": "110100",
-			"children": [{
-				"label": "东城区",
-				"value": "110101",
-				"children": []
-			}]
-		}]
-	}]
-
-	
-
-
-
-
+    @observable citylist = [{
+        "label": "北京市",
+        "value": "110000",
+        "children": [{
+            "label": "北京市",
+            "value": "110100",
+            "children": [{
+                "label": "东城区",
+                "value": "110101",
+                "children": []
+            }]
+        }]
+    }];
 }
+
 export default Store;
