a50f9f605f0a9b028f9406ed0b89310a214909d3
quanxin <quanxin@pujitech.com>
2021-01-20 10:57:47 +0800

    glob

--- src/Components/Phasetwo/Article/Component.js
+++ src/Components/Phasetwo/Article/Component.js
@@ -13,33 +13,37 @@ export default class PhasetwoArticle extends React.Component {
         // 项目家书 - 65
         // 工程进展 - 66
         // 社区文化 - 67
-        window.setWindowTitle("加载中")
-        this.props.actions.actionsPhasetwoArticle.setnoticeDetail(this.props.match.params.id)
-        let that=this;
-        window.JQ('.Components-Notice-a').on("click", "a", function () { 
+        window.setWindowTitle("加载中");
+        const {actionsPhasetwoArticle} = this.props.actions;
+        actionsPhasetwoArticle.init();
+        actionsPhasetwoArticle.setnoticeDetail(this.props.match.params.id);
+        window.JQ('.Components-Notice-a').on("click", "a", function (){
             window.location.href = window.JQ(this).attr('hrefnew')
         });
     }
-    componentWillReceiveProps(newProps) {
+
+    componentWillReceiveProps(newProps){
         if (this.props.match.params.id !== newProps.match.params.id) {
             this.props.actions.actionsPhasetwoArticle.setnoticeDetail(newProps.match.params.id)
         }
     }
+
     componentDidUpdate(){
-        let artA=window.JQ('.Components-Notice-a a')
-        for (let i = 0;artA.length>i;i++){
+        let artA = window.JQ('.Components-Notice-a a')
+        for (let i = 0; artA.length > i; i++) {
             let artAhref = artA.eq(i).attr('href')
             artA.eq(i).attr('hrefnew', artAhref)
             //artA.eq(i).removeAttr('href')
             artA.eq(i).attr('href', "javascript:void(0);")
         }
     }
-    render() {
-        const { store, actions, history} = this.props;
-        const { storePhasetwoArticle} = store;
-        const { actionsPhasetwoArticle} = actions;
-        const { readText, test1 } = actionsPhasetwoArticle;
-        const { setnoticeDetail } = storePhasetwoArticle
+
+    render(){
+        const {store, actions, history} = this.props;
+        const {storePhasetwoArticle} = store;
+        const {actionsPhasetwoArticle} = actions;
+        const {readText, test1} = actionsPhasetwoArticle;
+        const {setnoticeDetail} = storePhasetwoArticle
         let urlobj = window.getQueryString();
         let title = "城市资讯"
         if (urlobj && urlobj.title == 33) {
@@ -55,32 +59,33 @@ export default class PhasetwoArticle extends React.Component {
         } else {
             title = "城市资讯"
         }
-        console.log("title", title)
-        window.setWindowTitle(title ? title :"加载中")
+        console.log("title", title);
+        window.setWindowTitle(title ? title : "加载中");
         return <div url={"/Notice"} className="Components-Notice-container article_img">
-            
             <div className="content-wrap">
                 <div className="content">
                     {/* {urlobj && urlobj.title == 32?"": */}
-                        <div className="top">
-                            <div className="title">{setnoticeDetail.title}</div>
-                            <div className="sub-title">
-                                <span>作者：{setnoticeDetail.author}</span>
-                                {/* <span className="date">{setnoticeDetail.createTime && setnoticeDetail.createTime.split(" ")[0]}</span> */}
-                            </div>
+                    <div className="top">
+                        <div className="title">{setnoticeDetail.title}</div>
+                        <div className="sub-title">
+                            <span>作者：{setnoticeDetail.author}</span>
+                            {/* <span className="date">{setnoticeDetail.createTime && setnoticeDetail.createTime.split(" ")[0]}</span> */}
                         </div>
+                    </div>
                     {/* } */}
-                    
+
                     <div className="center Components-Notice-a">
                         <RichTextDisplay>
-                            {setnoticeDetail&&setnoticeDetail.content}
+                            {setnoticeDetail && setnoticeDetail.content}
                         </RichTextDisplay>
-                        
+
                     </div>
-                    {setnoticeDetail.originalLink&&
-                        <div className={"readTextc"} onClick={() => { readText(setnoticeDetail.originalLink, history) }}>阅读原文</div>
+                    {setnoticeDetail.originalLink &&
+                    <div className={"readTextc"} onClick={() => {
+                        readText(setnoticeDetail.originalLink, history)
+                    }}>阅读原文</div>
                     }
-                    
+
                 </div>
             </div>
         </div>;
