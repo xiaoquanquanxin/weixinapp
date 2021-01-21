b46c2849613bdaad17fe397ac5b976131ff20b78
quanxin <quanxin@pujitech.com>
2021-01-21 14:25:47 +0800

    glob

--- src/Components/Phasetwo/Activity/List/Store.js
+++ src/Components/Phasetwo/Activity/List/Store.js
@@ -1,13 +1,15 @@
-import { observable, action } from 'mobx';
+import {observable, action} from 'mobx';
+
 // 定义数据结构
 class Store {
     //使用 observable decorator
-    @observable tip='我是模板';
-    @observable actListdata=[]
+    @observable tip = '我是模板';
+    @observable actListdata = [];
 
-    @observable refreshing=false
-    @observable tabval=1
-    @observable height = document.documentElement.clientHeight - 0
-    @observable actbottom = 2
+    @observable refreshing = false;
+    @observable tabval = null;
+    @observable height = +document.documentElement.clientHeight;
+    @observable actbottom = 2;
 }
+
 export default Store;
