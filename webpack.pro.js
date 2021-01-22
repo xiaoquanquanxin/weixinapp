141b5654d6f0b1e301167fbe7ba9a8f710ced304
quanxin <quanxin@pujitech.com>
2021-01-22 15:20:11 +0800

    glob

--- webpack.pro.js
+++ webpack.pro.js
@@ -1,7 +1,7 @@
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = merge(common, {
     devtool: 'inline-source-map',
     entry: {
