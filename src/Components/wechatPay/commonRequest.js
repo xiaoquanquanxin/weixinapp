1a27a9bf4556e9039f9657c5ef8ebdc26e338c5c
quanxin <quanxin@pujitech.com>
2021-01-27 19:29:44 +0800

    glob

--- src/Components/wechatPay/commonRequest.js
+++ src/Components/wechatPay/commonRequest.js
@@ -18,4 +18,48 @@ export const requestGetFeeItem = async (pmdsRoomId, cmdsId) => {
     })
 };
 
+//  拿微信缴费签名
+export const weChatPayAdvanceFn = async (mchOrderNo, amount) => {
+    return await new Promise((resolve, reject) => {
+        const userInfo = JSON.parse(window.getLocalData('userInfo') || '{}');
+        const params = {
+            //  自定义商户ID，公众号支付传10000000
+            mchId: '10000000',
+            //  商户订单号-从物管接口返回的数据取
+            mchOrderNo,
+            //  渠道id,公众号传"WX_JSAPI"
+            channelId: "WX_JSAPI",
+            //  金额
+            amount,
+            //  任意ip
+            clientIp: "192.168.100.128",
+            //  设备
+            device: (window.OSInfo() === "ios") ? 'ios' : 'Android',
+            //  openId-从微信授权数据里取
+            openId: userInfo.openId,
+        };
+        console.log('wechatPayAdvance的参数是', params);
+        window.JQ.ajax({
+            type: "post",
+            url: `${ipUri["/opi"]}/live/wechatPayAdvance`,
+            data: {params: JSON.stringify(params)},
+            success: (result) => {
+                resolve(result);
+            },
+        })
+    });
+};
 
+//  转json
+export const transformWechatPayData = (data) => {
+    return data;
+    const {appid, nonce_str, prepay_id, sign} = data;
+    return {
+        timeStamp: new Date().getTime(),
+        package: `prepay_id=${prepay_id}`,
+        nonceStr: nonce_str,
+        signType: 'MD5',
+        appId: appid,
+        paySign: sign,
+    }
+};
