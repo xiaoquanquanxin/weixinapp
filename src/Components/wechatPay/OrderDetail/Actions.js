a740d6dc567153a63bdaea239e02bbb12d61a676
quanxin <quanxin@pujitech.com>
2021-01-28 15:32:03 +0800

    glob

--- src/Components/wechatPay/OrderDetail/Actions.js
+++ src/Components/wechatPay/OrderDetail/Actions.js
@@ -5,7 +5,7 @@ import {zeroFill} from "../../../../lib/utils/number";
 import {
     getBrandWCPayRequestFn,
     requestWeChatPayAdvanceFn,
-    requestGetTranStatusFn, requestGetPmdRoomsFn
+    requestGetTranStatusFn, requestGetPmdRoomsFn, requestCompletePaidOrderFn
 } from "../commonRequest";
 
 class Actions {
@@ -410,26 +410,7 @@ class Actions {
     @action
     completePaidOrder = async () => {
         const store = this.store;
-        const result = await new Promise((resolve, reject) => {
-            const updateTime = new Date().format('yyyy-MM-dd hh:mm:ss');
-            store.updateTime = updateTime;
-            let data = {
-                transactionId: store.transactionid,
-                updateTime,
-                //  服务端处理
-                payMethod: ''
-            };
-            window.JQ.ajax({
-                crossDomain: true,
-                type: "post",
-                url: `${ipUri["/bpi"]}/completePaidOrder.do`,
-                contentType: "application/x-www-form-urlencoded",
-                data: {'json': JSON.stringify(data)},
-                success: (result) => {
-                    resolve(result);
-                },
-            })
-        });
+        const result = await requestCompletePaidOrderFn(store.transactionid);
         const {code} = result;
         return code === 2000;
     }
