413072ccb9a76f90369be3a426c67c02dd8a8851
quanxin <quanxin@pujitech.com>
2021-02-01 17:32:38 +0800

    glob

--- src/Components/Certification/FamilyMembers/Store.js
+++ src/Components/Certification/FamilyMembers/Store.js
@@ -1,33 +1,27 @@
-import { observable, action } from 'mobx';
+import {observable, action} from 'mobx';
+
 // 定义数据结构
 class Store {
     //使用 observable decorator
-    @observable tip='我是模板';
-
-/*获取用户信息*/
-	@observable useInfo={
-	};
-
-
-	@observable userFamily=[
-		{
-			id: '',
-			fullName:'',
-			sex: '',
-			phoneNo: '',
-			identityNo: '',
-			birthday: '',
-			userType: '',
-			editStatus:false
-		},
-	];
-	@observable getRoomInfo=[ ];
-	@observable roomId=-1
-	@observable roomName=-1
-	@observable userType=-1
-
-
-
-
+    @observable tip = '我是模板';
+    /*获取用户信息*/
+    @observable useInfo = {};
+    @observable userFamily = [
+        {
+            id: '',
+            fullName: '',
+            sex: '',
+            phoneNo: '',
+            identityNo: '',
+            birthday: '',
+            userType: '',
+            editStatus: false
+        },
+    ];
+    @observable getRoomInfo = [];
+    @observable roomId = -1;
+    @observable roomName = -1;
+    @observable userType = -1;
 }
+
 export default Store;
