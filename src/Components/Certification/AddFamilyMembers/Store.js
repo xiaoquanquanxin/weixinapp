83de65e4ee505cd345676a3ac04a69378d301e64
quanxin <quanxin@pujitech.com>
2021-02-01 17:45:27 +0800

    glob

--- src/Components/Certification/AddFamilyMembers/Store.js
+++ src/Components/Certification/AddFamilyMembers/Store.js
@@ -1,32 +1,35 @@
-import { observable, action } from 'mobx';
+import {observable, action} from 'mobx';
+
 // 定义数据结构
 class Store {
     //使用 observable decorator
-    @observable tip='我是模板';
+    @observable tip = '我是模板';
 
-	@observable AddFamilyMembers={
-        fullName:'',                 //姓名
+    @observable AddFamilyMembers = {
+        fullName: '',                 //姓名
         phoneNo: '',                 //电话号码
         identityNo: '',           //身份证
         sex: '',                //姓别
         birthday: '',              //出生年月
         userType: '',       //客户类型
-        roomName:''
+        roomName: ''
     };
-	@observable addFamilyUser='';
-    @observable userFamily=[];
-    
+    @observable addFamilyUser = '';
+    @observable userFamily = [];
+
     @observable Foldval = false;
-    @observable ProjectSelectval = -1
-    @observable ProjectSelecData = []
+    @observable ProjectSelectval = -1;
+    @observable ProjectSelecData = [];
 
-    @observable getRoomInfo=[]
-    @observable roomName=""
-    @observable ProjectSelectvalarr=[]
-    @observable roomIdarr=[]
+    @observable getRoomInfo = [];
+    @observable roomName = "";
+    @observable ProjectSelectvalarr = [];
+    @observable roomIdarr = [];
 
-    @observable colorStyle=false
-    @observable getUserInfoByParamval=0
-
+    @observable colorStyle = false;
+    @observable getUserInfoByParamval = 0;
+    //  当前房间的custId
+    @observable custId = -1;
 }
+
 export default Store;
