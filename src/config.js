d5c79a7b6f9a05995443fb7268ff1d2c8bfb752d
quanxin <quanxin@pujitech.com>
2021-02-05 16:27:32 +0800

    glob

--- src/config.js
+++ src/config.js
@@ -1,31 +1,28 @@
 import VConsole from 'vconsole';
-// console.log(IS_CLIENT);
-if (IS_CLIENT) {
-    new VConsole();
-}
 //  2020年12月29日19:41:37，新增接口配置
 export const ipUri = {
-    //  todo    wechat-pay
     //  服务端源码接口
-    // '/mpi': 'https://wygzh-test.seedland.cc/wechat-mobile/',
     // '/mpi': 'http://192.168.100.208:5080/wechat-mobile/',
-    '/mpi': '/mpi',
+    // '/mpi': '/mpi',
 
     //  少杰 端口
-    //  todo    wechat-pay
-    // '/opi': 'https://wygzh-test.seedland.cc/v3/api',
     // '/opi': 'http://hachi-pay.mynatapp.cc/api',
-    '/opi': '/opi',
+    // '/opi': '/opi',
 
-    //  todo    wechat-pay
-    // '/bpi': 'https://wygzh-test.seedland.cc/v3/api',
     // '/bpi': 'http://192.168.100.208:3080',
-    '/bpi': '/bpi',
+    // '/bpi': '/bpi',
 
     //  todo    wechat-pay
     // '/workorder': 'http://asm-test.seedland.cc:8084/life-web/sso/api/workorder',
     '/workorder': '/workorder',
 };
+if (IS_CLIENT) {
+    new VConsole();
+    ipUri['/mpi'] = 'https://wygzh-test.seedland.cc/wechat-mobile/';
+    ipUri['/opi'] = 'https://wygzh-test.seedland.cc/v3/api';
+    ipUri['/bpi'] = 'https://wygzh-test.seedland.cc/v3/api';
+}
+
 
 const host = ipUri["/mpi"];
 console.log(`api前面的静态资源前缀 ${host}`);
