d3dc2b5e3cc8fff2157ead92ffd2e1544f5d9ea2
quanxin <quanxin@pujitech.com>
2021-02-05 15:29:54 +0800

    glob

--- src/Components/Phasetwo/CityInformation/List/Store.js
+++ src/Components/Phasetwo/CityInformation/List/Store.js
@@ -1,8 +1,9 @@
-import { observable, action } from 'mobx';
+import {observable, action} from 'mobx';
+
 // 定义数据结构
 class Store {
     //使用 observable decorator
-    @observable tip='我是模板';
+    @observable tip = '我是模板';
     @observable companyName = [
         {
             label: '广州省·广州市',
@@ -13,18 +14,19 @@ class Store {
             value: '实地·北京常春藤',
         },
     ];
-    @observable companyNameValue = '广州省·广州市'
-    @observable cityId=""
-    @observable cityName = ""
-    @observable cityList=[]
-    @observable CityListdata=[]
-    @observable provincesList=[]
-    @observable provincesId = ""
-    @observable provincesName = ""
-    @observable col=1;//列数
+    @observable companyNameValue = '广州省·广州市';
+    @observable cityId = "";
+    @observable cityName = "";
+    @observable cityList = [];
+    @observable CityListdata = [];
+    @observable provincesList = [];
+    @observable provincesId = "";
+    @observable provincesName = "";
+    @observable col = 1;//列数
     @observable allList = [];
-    @observable refreshing = false
-    @observable actbottom = 2
+    @observable refreshing = false;
+    @observable actbottom = 2;
     @observable height = +document.documentElement.clientHeight;
 }
+
 export default Store;
